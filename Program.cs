3e56ae128742463615d7618ed1ea033881da4cd4
Marcelo <mdbezerra16@gmail.com>
2025-03-24 12:04:35 -0300

    Lendo arquivo e trabalhando com Buffer e UTF8

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using ByteBankIO;
 
 namespace Trabalhando_com_arquivos
 {
@@ -6,8 +9,32 @@ namespace Trabalhando_com_arquivos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            var enderecoDoArquivo = "contas.txt";
+            var numeroDeBytesLidos = -1;
+
+            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024]; //1KB
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+        }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            var utf8 = new UTF8Encoding();
+
+            var texto = utf8.GetString(buffer);
+            Console.WriteLine(texto);
+
+            /*foreach (var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write(" ");
+            }*/
         }
     }
 }
