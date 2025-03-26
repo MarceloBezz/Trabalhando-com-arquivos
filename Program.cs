a051059e59363b5d7c34dd44a45421b66ce7922b
MarceloBezz <mdbezerra16@gmail.com>
2025-03-26 08:30:12 -0300

    Classe File

--- Program.cs
+++ Program.cs
@@ -9,8 +9,19 @@ namespace Trabalhando_com_arquivos
     {
         static void Main(string[] args)
         {
-            EscritaBinaria();
-            LeituraBinaria();
+            // var linhas = File.ReadAllLines("contas.txt");
+            // Console.WriteLine(linhas.Length);
+
+            // foreach (var linha in linhas)
+            // {
+            //     Console.WriteLine(linha);
+            // }
+
+            var bytesArquivo = File.ReadAllBytes("contas.txt");
+            Console.WriteLine($"Arquivo contas.txt possui {bytesArquivo.Length} bytes");
+
+            File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllText");
+
             Console.WriteLine("Aplicação finalizada!");
         }
     }
